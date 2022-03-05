aa61a1c7c032fe001d789efb007600e7831c5347
itacdonev <itacdonev@gmail.com>
2022-03-05 12:54:16 +0100

    Add DATA_DIR_ paths

---
+++ src/config.py
@@ -0,0 +1,8 @@
+"""Global variables to be used for the project.
+"""
+
+# GLOBAL VARIABLES
+DATA_DIR = '../input/'
+DATA_DIR_TRAIN = '../input/train_features/'
+DATA_DIR_TEST = '../input/test_features/'
+DATA_DIR_VAL = '../input/val_features/'

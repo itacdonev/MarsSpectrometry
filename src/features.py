cbcce70d32b7925848fc29a3f4bd957c2d5a586b
itacdonev <itacdonev@gmail.com>
2022-03-15 13:06:52 +0100

    Fix features_iontemp_abun()

--- src/features.py
+++ src/features.py
@@ -1,7 +1,7 @@
 """Feature engineering"""
 
-import enum
 import pandas as pd
+import numpy as np
 from src import config, preprocess
 from tqdm import tqdm
 
@@ -9,11 +9,12 @@ from tqdm import tqdm
 
 # BENCHMARK FEATURES
 # Bin the temp and compute processes max abundance for each bin
-def bin_temp_abund(df_sample, sample_name):
+def bin_temp_abund(df_sample, sample_name:str):
     """
     Create equal bins for temperature
     Compute max for relative abundance for each bin and ion type
     Return a dataframe where rows are samples and columns are temp-ion bins
+    Computed on sample level
     """
     # Create a series of temperature bins
     temprange = pd.interval_range(start=-100, end=1500, freq=100)
@@ -24,6 +25,7 @@ def bin_temp_abund(df_sample, sample_name):
     
     # Compute max relative abundance
     ht = df_sample.groupby(['m/z', 'temp_bin'])['abun_minsub_scaled'].agg('max').reset_index()
+    ht = ht.replace(np.nan, 0)
     ht['sample_id'] = sample_name
     ht.columns = ['Ion_type', 'temp_bin', 'max_rel_abund', 'sample_id']
     
@@ -33,27 +35,51 @@ def bin_temp_abund(df_sample, sample_name):
     ht_pivot.columns = ht_pivot.columns.map(lambda x: '_'.join([str(i) for i in x]))
     ht_pivot = ht_pivot.add_prefix('Ion_')
     ht_pivot = ht_pivot.reset_index().rename_axis(None, axis=1)
-    ht_pivot.drop('sample_id', axis = 1, inplace=True)
+
+    #ht_pivot.drop('sample_id', axis = 1, inplace=True)
     
-    return ht_pivot.fillna(0)
+    return ht_pivot
 
 
-def features_iontemp_abun(df_meta, sample_list:list):
+def features_iontemp_abun(df_meta, sample_list):
     
     # Initialize a table to store computed values
     dt = pd.DataFrame(dtype='float64')
     
+    #ion_temp_dict = {}
+    
     # Loop over all sample_id and compute. Add computation to dt.
     print(f'Number of samples: {len(sample_list)}')
-    for i, sample_name in tqdm(enumerate(sample_list)):
+    for i in sample_list:
+        sample_name = df_meta.iloc[i]['sample_id']
+        #print(sample_name)
         df_sample = preprocess.get_sample(df_meta, i)
         
         ht_pivot = bin_temp_abund(df_sample, sample_name)
-        dt = pd.concat([dt, ht_pivot], ignore_index=True)
+        #ion_temp_dict[sample_name] = ht_pivot
+        dt = pd.concat([dt, ht_pivot])
     
+    dt = dt.set_index('sample_id')
+    
+    # Rename columns
+    t_cols = dt.columns
+    remove_chars = "(,]"
+    for char in remove_chars:
+        t_cols = [i.replace(char,'') for i in t_cols]
+    t_cols = [i.replace(' ','_') for i in t_cols]
+    dt.columns = t_cols
+
     return dt
         
-        
+# === TARGET ENCODING ===
+
+
+       
+
+
+
+
+
 
 # Compute average abundance for each sample
 def compute_abundance_per_ion(df_sample, sample_name:str, stat:str):

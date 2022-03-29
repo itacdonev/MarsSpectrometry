bdbd7fc0f150c4534a9df53c0f1a19b85391cdf7
itacdonev <itacdonev@gmail.com>
2022-03-29 12:53:08 +0200

    plot_mz_ts(), plot_ms()

--- src/describe_data.py
+++ src/describe_data.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -31,4 +32,35 @@ def annotate_countplot(sp, df: pd.DataFrame(),
         sp.text(p.get_x() + p.get_width()/2., 
                 height + len(df) * perc_height, height,
                 ha = 'center', fontsize = font_size)
-        
+
+         
+def plot_mz_ts(df_sample,col_to_plot:str):
+    
+    ions = df_sample['m/z'].unique().tolist()
+    
+    for ion in ions:
+        plt.plot(df_sample[df_sample['m/z'] == ion]['temp'],
+                 df_sample[df_sample['m/z'] == ion][col_to_plot], 
+                 label=ion)
+    #plt.legend()
+    plt.xlabel('Temperature')
+    plt.ylabel(col_to_plot)
+    plt.title('Time series plot for each m/z ion')
+    sns.despine()
+    plt.show()
+    
+    
+def plot_ms(df_sample, col_to_plot:str, target:str=None, sample_id:str=None):
+    """Plot mass spectra."""
+    
+    # List of ions present in the sample
+    ion_list = df_sample['m/z'].unique().tolist()
+       
+    for ion in ion_list:
+        temp = df_sample[df_sample['m/z'] == ion]
+        y = temp[col_to_plot].max()
+        plt.plot([ion,ion], [0,y], c='#2D4B73')
+    
+    plt.title(f'Sample: {sample_id}   Target: {target}')
+    plt.xlabel('m/z ion')
+    sns.despine()
